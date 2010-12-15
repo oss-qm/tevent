2704f35a369bed8e4cea46580c651d2dcedde127
Jelmer Vernooij <jelmer@samba.org>
2010-12-15 15:33:00 +0100

    New upstream snapshot.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
-GIT_URL=$1
-REFSPEC=$2
-shift 2
+#!/bin/bash -e
 
 if [ -z "$GIT_URL" ]; then
 	GIT_URL=git://git.samba.org/samba.git
 fi
 
-TEVENTTMP=$TMPDIR/$RANDOM.tevent.git
+TEVENTTMP=`mktemp -d`
 git clone --depth 1 $GIT_URL $TEVENTTMP
 if [ ! -z "$REFSPEC" ]; then
 	pushd $TEVENTTMP
@@ -15,10 +12,9 @@ if [ ! -z "$REFSPEC" ]; then
 	popd
 fi
 pushd $TEVENTTMP/lib/tevent
-./autogen-waf.sh
 ./configure
 make dist
 popd
-version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
-mv $TEVENTTMP/lib/tevent/tevent-*.tar.gz tevent-$version.tar.gz
+version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9]\+$//' )
+mv $TEVENTTMP/lib/tevent/tevent-*.tar.gz tevent_$version.orig.tar.gz
 rm -rf $TEVENTTMP
