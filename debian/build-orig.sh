8125d6fa560a4aaed7f14c80c5287a7aa6d17fc6
Jelmer Vernooij <jelmer@samba.org>
2010-12-21 17:02:20 +0100

    Use new upstream snapshot.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
 
-if [ -z "$GIT_URL" ]; then
-	GIT_URL=git://git.samba.org/samba.git
+if [ -z "$SAMBA_GIT_URL" ]; then
+	SAMBA_GIT_URL=git://git.samba.org/samba.git
 fi
 
 TEVENTTMP=`mktemp -d`
-git clone --depth 1 $GIT_URL $TEVENTTMP
-if [ ! -z "$REFSPEC" ]; then
-	pushd $TEVENTTMP
-	git checkout $REFSPEC || exit 1
-	popd
+if [ -d $SAMBA_GIT_URL/.bzr ]; then
+	bzr co --lightweight $SAMBA_GIT_URL $TEVENTTMP
+else
+	git clone --depth 1 $SAMBA_GIT_URL $TEVENTTMP
 fi
 pushd $TEVENTTMP/lib/tevent
 ./configure
